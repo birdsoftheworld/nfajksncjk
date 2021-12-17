df6b30da995bb8b6c4d21664a272d742fe317416
birdsoftheworld <53488356+birdsoftheworld@users.noreply.github.com>
2021-12-17 11:36:19 -0800

    a

---
+++ js/main.js
@@ -0,0 +1,40 @@
+import { GameState } from "./state/state.js";
+
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+ctx.imageSmoothingEnabled = false;
+
+let width = 10;
+let height = 10;
+let cwidth = 100;
+let cheight = 100;
+let size = cwidth / width;
+let state = new GameState(width, height);
+
+function draw() {
+    ctx.clearRect(0, 0, cwidth, cheight);
+    ctx.fillStyle = "red";
+    ctx.fillRect(state.foodX * size, state.foodY * size, size, size);
+    let segments = state.playerSegments;
+    ctx.fillStyle = "black";
+    for(let i = 0; i < segments.length; i++) {
+        let segment = segments[i];
+        ctx.fillRect(segment[0] * size, segment[1] * size, size, size);
+    }
+}
+
+function update() {
+    state.movePlayer();
+}
+
+function onKeyDown(e) {
+    event.setCancelled(true);
+    let keys = { "w": 3, "a": 2, "s": 1, "d": 0 };
+    let dir = keys[e.key];
+    if(dir != undefined) {
+        state.playerDir = dir;
+    }
+}
+
+window.requestAnimationFrame(draw);
+window.addEventListener("keydown", onKeyDown);
---
+++ js/state/state.js
@@ -0,0 +1,56 @@
+let directions = [ [1, 0], [0, 1], [-1, 0], [0, -1] ];
+
+class GameState {
+    constructor(width, height) {
+        this.playerLength = 1;
+        this.playerDir = -1;
+        this.playerX = 1;
+        this.playerY = 1;
+        this.playerSegments = [];
+        this.playerSegments.push([playerX, playerY]);
+        this.width = width;
+        this.height = height;
+        this.lost = false;
+        this.spawnFood();
+    }
+
+    gameOver() {
+        this.lost = true;
+    }
+
+    movePlayer() {
+        if(this.lost) return;
+        if(this.playerDir == -1) return;
+        let dir = directions[this.playerDir];
+        let newX = dir[0] + this.playerX;
+        let newY = dir[1] + this.playerY;
+        if(newX < 0 || newY < 0 || newX >= this.width || newY >= this.height) {
+            this.gameOver();
+            return;
+        }
+
+        for(let i = 0; i < this.playerSegments.length; i++) {
+            let segment = this.playerSegments[i];
+            if(segment[0] == newX && segment[1] == newY) {
+                this.gameOver();
+                return;
+            }
+        }
+
+        if(newX == this.foodX && newY == this.foodY) {
+            this.playerLength++;
+        } else {
+            this.pop();
+        }
+        this.playerX = newX;
+        this.playerY = newY;
+        this.unshift([this.playerX, this.playerY]);
+    }
+
+    spawnFood() {
+        this.foodX = Math.floor(Math.random() * this.width);
+        this.foodY = Math.floor(Math.random() * this.height);
+    }
+}
+
+export { GameState };
